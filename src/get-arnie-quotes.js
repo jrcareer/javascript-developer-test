88e0df0ce2a00e571e31de524e7abb24665b1ace
jrcareer <102664843+jrcareer@users.noreply.github.com>
2022-03-30 22:02:58 +1100

    Update get-arnie-quotes.js
    Add getArnieQuotes functionality

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,27 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const Status = {
+  SUCCESS: 200,
+  FAILURE: 500
 };
+
+const getArnieQuotes = async (urls) => {
+  const results = [];
+
+  await Promise.all(urls.map(async url => {
+    const response = await httpGet(url);
+
+    const { message } = JSON.parse(response.body)
+
+    if (response.status === Status.SUCCESS) results.push({ 'Arnie Quote': message })
+
+    // For mock purposes, will always be 200 or 500 however README states if it is not 200, not specifically if it is a 500
+    else results.push({ FAILURE: message })
+  }))
+
+  return results;
+};
+
 
 module.exports = {
   getArnieQuotes,
